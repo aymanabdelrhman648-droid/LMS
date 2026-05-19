4e76485c12eef6c53b67875a4937d9c5c6374d4f
Abdelrhman <aymanabdelrhman648@gmail.com>
2026-05-20 00:26:24 +0300

    move mongo uri to env

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,12 +1,11 @@
 import mongoose from "mongoose";
 
 export const connectdb = async () => {
-  await mongoose.connect(
-    await mongoose.connect(
-  'mongodb+srv://aymanabdelrhman648_db_user:Z2YSVsbiC3ngRTOC@cluster1.fiitwhz.mongodb.net/LMS?retryWrites=true&w=majority&appName=Cluster1'
-)
-  )
-  .then(() => {
-    console.log('DB Connected');
-  });
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+
+    console.log("DB Connected");
+  } catch (error) {
+    console.log("Mongo Error:", error.message);
+  }
 };
